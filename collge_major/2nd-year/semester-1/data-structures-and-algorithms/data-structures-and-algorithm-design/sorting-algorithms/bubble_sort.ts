6a2190b21d99df3ccdcea601d790094e0601ec3b
ernkeyz <exconzy26@gmail.com>
2024-03-24 20:55:50 +0000

    bubble sort

---
+++ collge_major/2nd-year/semester-1/data-structures-and-algorithms/data-structures-and-algorithm-design/sorting-algorithms/bubble_sort.ts
@@ -0,0 +1,22 @@
+const bubbleSort = <T>(arr: T[]): T[] => {
+  const len = arr.length;
+  let swapped: Boolean;
+
+  do {
+    swapped = false;
+    for (let i = 0; i < len - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+        swapped = true;
+      }
+    }
+  } while (swapped);
+
+  return arr;
+};
+
+// Example usage:
+const unsortedArray = [5, 3, 8, 1, 2];
+console.log("Unsorted Array:", unsortedArray); // Output: [5, 3, 8, 1, 2]
+const sortedArray = bubbleSort(unsortedArray);
+console.log("Sorted Array:", sortedArray); // Output: [1, 2, 3, 5, 8]
