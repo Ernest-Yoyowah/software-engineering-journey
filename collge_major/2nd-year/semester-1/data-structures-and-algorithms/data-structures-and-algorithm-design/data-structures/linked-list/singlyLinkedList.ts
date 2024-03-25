9d1a15efac3887f9e4cabadb89b11b0c0de5127c
ernkeyz <exconzy26@gmail.com>
2024-03-25 21:46:45 +0000

    singly linked list data structure

---
+++ collge_major/2nd-year/semester-1/data-structures-and-algorithms/data-structures-and-algorithm-design/data-structures/linked-list/singlyLinkedList.ts
@@ -0,0 +1,47 @@
+class ListNode<T> {
+  public value: T;
+  public next: ListNode<T> | null;
+
+  constructor(value: T) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList<T> {
+  private head: ListNode<T> | null;
+  private tail: ListNode<T> | null;
+
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  // Insert a new node at the end of the list
+  append(value: T): void {
+    const newNode = new ListNode(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail!.next = newNode;
+      this.tail = newNode;
+    }
+  }
+
+  // Display the elements of the list
+  display(): void {
+    let current: ListNode<T> | null = this.head;
+    while (current !== null) {
+      console.log(current.value);
+      current = current.next;
+    }
+  }
+}
+
+// Example usage:
+const linkedList = new SinglyLinkedList<number>();
+linkedList.append(1);
+linkedList.append(2);
+linkedList.append(3);
+linkedList.display(); // Output: 1, 2, 3
