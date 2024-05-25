c0b7a28c7822361672cb67d25ab28e7b9dc3a425
Ernest Yoyowah <ernestniiyoyowah@gmail.com>
2024-05-25 23:16:12 +0000

    warm up exercise - searhing algorithm

---
+++ collge_major/2nd-year/semester-1/data-structures-and-algorithms/data-structures-and-algorithm-design/searching-algorithms/linear_search.ts
@@ -0,0 +1,12 @@
+const linearSearch = (arr: number[], target: number): number => {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === target) {
+      return i;
+    }
+  }
+  return -1;
+};
+
+const array: number[] = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
+
+linearSearch(array, 3);
