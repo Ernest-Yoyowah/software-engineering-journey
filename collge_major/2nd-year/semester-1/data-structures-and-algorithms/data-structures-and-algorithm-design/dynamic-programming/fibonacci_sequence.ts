b7b1e20c61328b3b48e1f64a04306d70d58fbb05
Ernest Yoyowah <ernestniiyoyowah@gmail.com>
2024-05-28 22:19:44 +0000

    update

---
+++ collge_major/2nd-year/semester-1/data-structures-and-algorithms/data-structures-and-algorithm-design/dynamic-programming/fibonacci_sequence.ts
@@ -0,0 +1,10 @@
+const fibonacciSequence = (n: number) => {
+  let fib: number[] = [0, 1];
+
+  for (let i = 2; i < n; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2];
+  }
+  return fib;
+};
+
+console.log(fibonacciSequence(5));
