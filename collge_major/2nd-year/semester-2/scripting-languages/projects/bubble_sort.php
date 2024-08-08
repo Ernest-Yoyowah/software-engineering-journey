42f2d9b962d3af1c56e144e57d06b926659e21bd
Ernest Yoyowah <ernestniiyoyowah@gmail.com>
2024-08-08 23:57:32 +0000

    bubble sort in php

---
+++ collge_major/2nd-year/semester-2/scripting-languages/projects/bubble_sort.php
@@ -0,0 +1,23 @@
+<?php
+function bubbleSort(&$arr) {
+    $n = count($arr) -1;
+    for ($i = 0; $i < $n; $i++) {
+        for ($j = 0; $j < $n - $i; $j++) {
+            if ($arr[$j] > $arr[$j + 1]) {
+                // Swap elements
+                // $temp = $arr[$j];
+                // $arr[$j] = $arr[$j + 1];
+                // $arr[$j + 1] = $temp;
+                [$arr[$j], $arr[$j + 1]] = [$arr[$j + 1], $arr[$j]];
+            }
+        }
+    }
+}
+
+// Example usage:
+$array = [64, 34, 25, 12, 22, 11, 90];
+bubbleSort($array);
+
+echo "Sorted array: ";
+print_r($array);
+?>
