7d20c3507e195463f312b98d7a94057bf33e9253
Ernest Yoyowah <ernestniiyoyowah@gmail.com>
2024-08-11 22:54:08 +0000

    update

--- collge_major/2nd-year/semester-1/data-structures-and-algorithms/data-structures-and-algorithm-design/data-structures/linked-list/singlyLinkedList.ts
+++ collge_major/2nd-year/semester-1/data-structures-and-algorithms/data-structures-and-algorithm-design/data-structures/linked-list/singlyLinkedList.ts
@@ -2,7 +2,7 @@ class ListNode<T> {
   public value: T;
   public next: ListNode<T> | null;
 
-  constructor(value: any) {
+  constructor(value: T) {
     this.value = value;
     this.next = null;
   }
