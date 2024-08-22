d9d97a7acd9882525f0dade73c71246c51fed2f1
Ernest Yoyowah <ernestniiyoyowah@gmail.com>
2024-08-22 19:38:23 +0000

    This class will represent a book in the library.

---
+++ collge_major/2nd-year/semester-2/object-oriented-analysis-design-and-programming/codebase/library-system-program/Book.ts
@@ -0,0 +1,35 @@
+export class Book {
+  private title: string;
+  private author: string;
+  private isbn: string;
+  private isAvailable: boolean;
+
+  constructor(title: string, author: string, isbn: string) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+    this.isAvailable = true;
+  }
+
+  borrow(): void {
+    if (this.isAvailable) {
+      this.isAvailable = false;
+      console.log(`${this.title} has been borrowed.`);
+    } else {
+      console.log(`${this.title} is not available.`);
+    }
+  }
+
+  returnBook(): void {
+    this.isAvailable = true;
+    console.log(`${this.title} has been returned.`);
+  }
+
+  getInfo(): string {
+    return `${this.title} by ${this.author} (ISBN: ${this.isbn})`;
+  }
+
+  getAvailability(): boolean {
+    return this.isAvailable;
+  }
+}
