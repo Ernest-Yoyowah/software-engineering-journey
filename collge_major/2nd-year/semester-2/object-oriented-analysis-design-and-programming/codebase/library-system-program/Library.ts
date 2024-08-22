f9a6fb267dcd77df4655ef85110c6978eb4d1faf
Ernest Yoyowah <ernestniiyoyowah@gmail.com>
2024-08-22 19:43:23 +0000

    This class manages the collection of books and members

---
+++ collge_major/2nd-year/semester-2/object-oriented-analysis-design-and-programming/codebase/library-system-program/Library.ts
@@ -0,0 +1,36 @@
+import { Book } from "./Book";
+import { Member } from "./Member";
+
+export class Library {
+  private books: Book[];
+  private members: Member[];
+
+  constructor() {
+    this.books = [];
+    this.members = [];
+  }
+
+  addBook(book: Book): void {
+    this.books.push(book);
+    console.log(`${book.getInfo()} added to the library.`);
+  }
+
+  addMember(member: Member): void {
+    this.members.push(member);
+    console.log(`${member.name} has been added as a member.`);
+  }
+
+  listAvailableBooks(): void {
+    console.log("Available books:");
+    this.books.forEach((book) => {
+      if (book.getAvailability()) {
+        console.log(book.getInfo());
+      }
+    });
+  }
+
+  listMembers(): void {
+    console.log("Library members:");
+    this.members.forEach((member) => console.log(member.name));
+  }
+}
