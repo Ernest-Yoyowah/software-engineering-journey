aa3f4a74f0669fd6d958d112ca99475dea41fcfa
Ernest-Yoyowah <ernestniiyoyowah@gmail.com>
2024-09-21 11:38:32 +0000

    TODO: Add books to the library

--- collge_major/2nd-year/semester-2/object-oriented-analysis-design-and-programming/codebase/library-system-program/index.ts
+++ collge_major/2nd-year/semester-2/object-oriented-analysis-design-and-programming/codebase/library-system-program/index.ts
@@ -4,7 +4,7 @@ import { Member } from "./Member";
 
 const library = new Library();
 
-// FIXME: Add books to the library
+// TODO: Add books to the library
 const book1 = new Book(
   "The Great Gatsby",
   "F. Scott Fitzgerald",
